70521835a025df9d17fb756b9bb920a2b8ed9dad
livan <livdevan@gmail.com>
2019-04-25 14:25:42 +0200

    promise with firestore trigger

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,38 @@
 import * as functions from "firebase-functions";
-
+import * as admin from "firebase-admin";
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
-//
-export const helloWorld = functions.https.onRequest((request, response) => {
-  console.log("Hello !");
-  response.send("Hello from Amiltone!");
-});
+admin.initializeApp();
+
+// export const onBostonWeatherUpdate = functions.firestore
+//   .document("cities-weather/VrWPTAlzgc5hHLEI0BQQ")
+//   .onUpdate(change => {
+//     const after = change.after.data();
+//     const payload = {
+//       data: {
+//         temp: String(after.temp),
+//         conditions: after.conditions
+//       }
+//     };
+//     return admin
+//       .messaging()
+//       .sendToTopic("weather_boston-ma-us", payload)
+//       .catch(error => console.error("FCM Failed", error));
+//   });
+
+export const getBostonWeather = functions.https.onRequest(
+  (request, response) => {
+    admin
+      .firestore()
+      .doc("cities-weather/boston - ma - us")
+      .get()
+      .then(snapshot => {
+        const data = snapshot.data();
+        response.send(data);
+      })
+      .catch(error => {
+        console.log(error);
+        response.status(500).send(error);
+      });
+  }
+);
